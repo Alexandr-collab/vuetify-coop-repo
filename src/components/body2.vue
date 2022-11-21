23faf73333e9ee0861a61f4dcbd1ed02014f2f73
Alexandr-collab <79284911633@yandex.ru>
2022-11-21 20:38:50 +0300

    xz
    asd

--- src/components/body2.vue
+++ src/components/body2.vue
@@ -1,9 +1,191 @@
 <template>
-    <v-container>
-        боди2
-    </v-container>
+    <v-container class="Noto Sans">
+    <div class="su">    
+<p class="ps"> Snap photos and share like never before </p>
+<p class="ss">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <br/>tempor incididunt ut labore</p>
+    <div class="sd">
+    <p class="as">
+        Sed ut perspiciatis
+    </p>
+    <p class="sa">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur.</p>
+    <p class="se">Lorem ipsum dolor</p>
+    <p class="st">Amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+    <p class="sr">Nemo enim ipsam</p>
+    <p class="sy">Consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.</p>
+</div>
+</div>
+</v-container>
   </template>
-  
+  <style>
+.ps{
+    
+width: 1392px;
+height: 72px;
+
+
+/* Sub-page H1 */
+
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 400;
+font-size: 48px;
+line-height: 72px;
+/* identical to box height, or 150% */
+
+text-align: center;
+letter-spacing: -0.015em;
+
+color: #000000;
+
+}
+.ss{
+width: 1392px;
+height: 100px;
+
+/* Body P Large */
+
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 400;
+font-size: 36px;
+line-height: 50px;
+/* or 139% */
+
+text-align: center;
+letter-spacing: -0.015em;
+
+color: rgba(0, 0, 0, 0.8);
+}
+.sd{
+    position: absolute;
+width: 622px;
+height: 480px;
+left: 80px;
+top: 552px;
+}
+.as{
+    
+
+
+
+width: 622px;
+height: 36px;
+
+
+/* Body H4 */
+
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 700;
+font-size: 24px;
+line-height: 36px;
+/* identical to box height, or 150% */
+
+letter-spacing: -0.015em;
+
+color: #000000;
+}
+.sa{
+    
+width: 622px;
+height: 60px;
+
+
+/* Body P */
+
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 30px;
+/* or 167% */
+
+letter-spacing: -0.015em;
+
+color: rgba(0, 0, 0, 0.8);
+}
+.se{
+   
+width: 622px;
+height: 36px;
+
+
+/* Body H4 */
+
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 700;
+font-size: 24px;
+line-height: 36px;
+/* identical to box height, or 150% */
+
+letter-spacing: -0.015em;
+
+color: #000000;
+}
+.sr{
+width: 622px;
+height: 36px;
+
+
+/* Body H4 */
+
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 700;
+font-size: 24px;
+line-height: 36px;
+/* identical to box height, or 150% */
+
+letter-spacing: -0.015em;
+
+color: #000000;
+}
+.st{
+width: 622px;
+height: 60px;
+
+
+/* Body P */
+
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 30px;
+/* or 167% */
+
+letter-spacing: -0.015em;
+
+color: rgba(0, 0, 0, 0.8);
+}
+.sy{
+width: 622px;
+height: 60px;
+
+
+/* Body P */
+
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 30px;
+/* or 167% */
+
+letter-spacing: -0.015em;
+
+color: rgba(0, 0, 0, 0.8);
+}
+.su{
+width: 1680px;
+height: 1282px;
+left: -11px;
+top: 2156px;
+
+background: #FFFFFF;
+}
+</style>
   <script setup>
   </script>
   
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,9 +1,83 @@
+
 <template>
-    <v-container>
-        хедер
-    </v-container>
+    <v-toolbar title="photo" class="titl">
+    <a class="tr" href="http://htmlbook.ru/content/kak-sdelat-ssylku">Features</a>
+    
+    <a  class="tr" href="http://htmlbook.ru/content/kak-sdelat-ssylku">Pricing</a>
+    
+    <a class="tr" href="http://htmlbook.ru/content/kak-sdelat-ssylku">Community</a>
+    
+    <a  class="tr" href="http://htmlbook.ru/content/kak-sdelat-ssylku">Support</a>
+    <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
+    
+    <v-btn id="ad1" 
+    dot
+    color="blue"
+    variant="outlined" class="hs">
+        Login In
+    </v-btn>
+
+    <v-btn 
+        color="blue"
+        dot
+    id="ad2" variant="flat" class="hs" >
+        Register
+    </v-btn>
+  </v-toolbar>
+
 </template>
-  
-<script setup>
+<style>
+.tr{
+    text-decoration: none;
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 15px;
+line-height: 18px;
+display: flex;
+align-items: center;
+text-align: center;
+letter-spacing: -0.015em;
+margin-left:60px ;
+
+/* Actionable Item */
+
+color: #18A0FB;
+}
+.hs{
+    text-decoration: none;
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 15px;
+line-height: 18px;
+display: flex;
+align-items: center;
+text-align: center;
+letter-spacing: -0.015em;
+margin-left:20px ;
+
+/* Actionable Item */
+
+color: #18A0FB;
+}
+
+.title{
+
+
+font-family: 'Comfortaa';
+font-style: normal;
+font-weight: 400;
+font-size: 28.4444px;
+line-height: 32px;
+/* identical to box height */
+border-color: #09a4ec ;
+color: #000000;
+}
+</style>
+<script >
 </script>
+
   
